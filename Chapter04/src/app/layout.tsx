e9f9cb1705d00b50eae3bfc19d7f9d6257947b38
Dohyun Kim <dkmqflx@gmail.com>
2023-04-04 23:31:08 +0900

    4.12 레이아웃으로 골격을!

--- Chapter04/src/app/layout.tsx
+++ Chapter04/src/app/layout.tsx
@@ -1,18 +1,31 @@
-import './globals.css'
-
+import './globals.css';
+import styles from './layout.module.css';
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <header className={styles.header}>
+          <h1>Demo Note</h1>
+          <nav className={styles.nav}>
+            <a href=''>Contact</a>
+            <a href=''>About</a>
+          </nav>
+        </header>
+
+        {children}
+      </body>
     </html>
-  )
+  );
 }
+
+// A layout is UI that is shared between routes.
+// /contact, /product로 이동해도 공통으로 위에 정의한 header 부분 존재한다
