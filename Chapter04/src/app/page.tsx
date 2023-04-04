42bc6be6e8748a8be7632a0b9800e9e275a555da
Dohyun Kim <dkmqflx@gmail.com>
2023-04-05 03:40:03 +0900

    5.3 서버 컴포넌트

--- Chapter04/src/app/page.tsx
+++ Chapter04/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import os from 'os'; // 노드 API
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  console.log('hello');
+  console.log(os.hostname());
   return <h1>페이지</h1>;
 }
