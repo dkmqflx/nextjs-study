aea2a6222d5f7993c1b29c0a5020c62b215bdc26
Dohyun Kim <dkmqflx@gmail.com>
2023-04-05 18:41:35 +0900

    6.9 Redirect

--- Chapter04/next.config.js
+++ Chapter04/next.config.js
@@ -11,6 +11,17 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true,
+        // 영원히 옮겨갔다는 것을 search engine에게 308 status 코드 보내준다
+        // 그래서 이 페이지 영원히 변경되었으니까 캐시해다서 음번에도 이 페이지 오면 똑같이 처리해도된다는 것을 알려준다
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
