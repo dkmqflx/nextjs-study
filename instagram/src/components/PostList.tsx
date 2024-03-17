5f4189c1c6ea901bd9eb1129c9732579c648d521
dkmqflx <dkmqflx@gmail.com>
2024-03-17 15:33:59 +0900

    13.12, 13, 14 - 포스트 목록

--- instagram/src/components/PostList.tsx
+++ instagram/src/components/PostList.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { SimplePost } from "@/model/post";
+import useSWR from "swr";
+
 export default function PostList() {
-  return <p>PostList</p>;
+  const { data: posts, isLoading: loading } =
+    useSWR<SimplePost[]>("/api/posts");
+  console.log(posts);
+
+  return (
+    <ul>{posts && posts.map((post) => <li key={post.id}>{post.text}</li>)}</ul>
+  );
 }
---
+++ instagram/src/service/posts.ts
@@ -0,0 +1,30 @@
+import { SimplePost } from "./../model/post";
+import { client, urlFor } from "./sanity";
+
+const simplePostProjection = `
+    ...,
+    "username": author->username,
+    "userImage": author->image,
+    "image": photo,
+    "likes": likes[]->username,
+    "text": comments[0].comment,
+    "comments": count(comments),
+    "id":_id,
+    "createdAt":_createdAt
+`;
+
+export async function getFollowingPostsOf(username: string) {
+  // 포스트를 작성한 사람이 지금 로그인한 유저이거나
+  // 또는 포스트를 작성한 사람이 유저의 팔로잉하고 있는 사람인 경우
+  return client
+    .fetch(
+      `*[_type =="post" && author->username == "${username}"
+          || author._ref in *[_type == "user" && username == "${username}"].following[]._ref]
+          | order(_createdAt desc){
+          ${simplePostProjection}
+        }`
+    )
+    .then((posts) =>
+      posts.map((post: SimplePost) => ({ ...post, image: urlFor(post.image) }))
+    );
+}
