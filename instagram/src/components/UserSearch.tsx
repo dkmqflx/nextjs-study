486e20f8866cf6075760cb9b495230a4062dbe45
dkmqflx <dkmqflx@gmail.com>
2024-03-20 00:17:00 +0900

    13.25 사용자 검색💡- 컴포넌트

--- instagram/src/components/UserSearch.tsx
+++ instagram/src/components/UserSearch.tsx
@@ -1,15 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { ProfileUser } from "@/model/user";
+import { FormEvent, useState } from "react";
 import useSWR from "swr";
+import GridSpinner from "./ui/GridSpinner";
 
 export default function UserSearch() {
-  // /api/search/${keyword}
-  // 검색하는 keyword가 있다면 /api/search/bob -> 유저네임이나, 네임
-  // 검색하는 keyword가 없다면 /api/search -> 전제 유저
   const [keyword, setKeyword] = useState("");
-  const { data, isLoading, error } = useSWR(`/api/search/${keyword}`);
-  console.log(data);
+  const {
+    data: users,
+    isLoading,
+    error,
+  } = useSWR<ProfileUser[]>(`/api/search/${keyword}`);
 
-  return <></>;
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          autoFocus
+          placeholder="Search for a username or name"
+          value={keyword}
+          onChange={(e) => setKeyword(e.target.value)}
+        />
+      </form>
+
+      {error && <p>무언가가 잘못 되었음 😜</p>}
+
+      {isLoading && <GridSpinner />}
+
+      {!isLoading && !error && users?.length === 0 && (
+        <p>찾는 사용자가 없음 😭</p>
+      )}
+
+      <ul>
+        {users &&
+          users.map((user) => (
+            <li key={user.username}>
+              <p>{user.username}</p>
+            </li>
+          ))}
+      </ul>
+    </>
+  );
 }
