efa39ddd8a2aa618b7c3623c83197bac6eb02a46
dkmqflx <dkmqflx@gmail.com>
2024-05-27 15:36:41 +0900

    14.15 코멘트💡- 상세화면 UI

--- instagram/src/components/PostDetail.tsx
+++ instagram/src/components/PostDetail.tsx
@@ -1,6 +1,7 @@
-import { FullPost, SimplePost } from "@/model/post";
+import useMe from "@/hooks/me";
+import useFullPost from "@/hooks/post";
+import { SimplePost } from "@/model/post";
 import Image from "next/image";
-import userSWR from "swr";
 import ActionBar from "./ActionBar";
 import Avatar from "./Avatar";
 import CommentForm from "./CommentForm";
@@ -9,12 +10,20 @@ import PostUserAvatar from "./PostUserAvatar";
 type Props = {
   post: SimplePost;
 };
-
 export default function PostDetail({ post }: Props) {
-  const { id, userImage, username, image, createdAt, likes } = post;
-  const { data } = userSWR<FullPost>(`/api/posts/${id}`);
+  const { id, userImage, username, image } = post;
+
+  const { post: data, postComment } = useFullPost(id);
+
+  const { user } = useMe();
+
   const comments = data?.comments;
-  const handlePostComment = (comment: string) => {};
+
+  const handlePostComment = (comment: string) => {
+    user &&
+      postComment({ comment, username: user.username, image: user.image });
+  };
+
   return (
     <section className="flex w-full h-full">
       <div className="relative basis-3/5">
@@ -27,10 +36,8 @@ export default function PostDetail({ post }: Props) {
           sizes="650px"
         />
       </div>
-
       <div className="w-full basis-2/5 flex flex-col">
         <PostUserAvatar image={userImage} username={username} />
-
         <ul className="border-t border-gray-200 h-full overflow-y-auto p-4 mb-1">
           {comments &&
             comments.map(
@@ -49,9 +56,7 @@ export default function PostDetail({ post }: Props) {
               )
             )}
         </ul>
-
         <ActionBar post={post} />
-
         <CommentForm onPostComment={handlePostComment} />
       </div>
     </section>
