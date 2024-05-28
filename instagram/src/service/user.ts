7d8a252a658b0c5d81db300a3518a5bddbc1ec01
dkmqflx <dkmqflx@gmail.com>
2024-05-28 11:24:10 +0900

    14.21, 22 최신기능💡- SSR 페이지 업댓

--- instagram/src/service/user.ts
+++ instagram/src/service/user.ts
@@ -68,7 +68,11 @@ export async function getUserForProfile(username: string) {
       "followers": count(followers),
       "posts": count(*[_type=="post" && author->username == "${username}"])
     }
-    `
+    `,
+      {},
+      {
+        cache: "no-store",
+      }
     )
     .then((user) => ({
       ...user,
